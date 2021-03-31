9f082e3ebbc125daa675f5b42f2dd703c89c5b98
Alexandre <alexandre.aude16@gmail.com>
2021-03-31 12:52:14 +0200

    close form
    required in form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,10 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelectorAll('.close');
+const firtName = document.querySelectorAll('.first');
+const lastName = document.querySelectorAll('.last');
+const textControl = document.querySelectorAll('.text-control');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +24,9 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// to close form 
+closeForm.forEach ((close) => close.addEventListener('click', toCloseForm));
+function toCloseForm() {
+    modalbg.style.display = "none";
+  }
 
